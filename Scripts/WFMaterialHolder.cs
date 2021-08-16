f9da9e6c04cfd225a3bb60a366610dfa14d7558e
whiteflare <skn9x2@gmail.com>
2021-08-16 13:55:13 +0900

    WFMaterialHolderを追加

---
+++ Scripts/WFMaterialHolder.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WFMaterialHolder : MonoBehaviour
+{
+#if UNITY_EDITOR
+    public Material[] keepMaterials;
+#endif
+}
